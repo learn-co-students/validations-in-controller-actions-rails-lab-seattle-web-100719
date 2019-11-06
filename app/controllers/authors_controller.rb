b278f8cecd5e07c996324a0dad3c0d4e9f974c55
Matt Gahrns <matthewgahrns@hotmail.com>
2019-11-06 11:40:51 -0800

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,9 +7,13 @@ class AuthorsController < ApplicationController
   end
 
   def create
+    #They had @author = Author.create!(author_params)
+    #create! will automatically throw errors according
+    #to your validations
     @author = Author.new(author_params)
 
     if @author.valid?
+      #have to save @author to database if valid
       @author.save
       redirect_to author_path(@author)
     else
